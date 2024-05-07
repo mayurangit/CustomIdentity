b92b2b1a1c35136fe4bff124eceba609b45180f0
DEll <DEll@DESKTOP-5JBT5P4>
2024-05-07 20:32:43 +0530

    Add project files.

---
+++ CustomIdentityApp/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CustomIdentityApp.Data
+{
+	public class AppDbContext : IdentityDbContext<IdentityUser>
+	{
+	}
+}
